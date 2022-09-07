6534a938466ec1d0d8c5fbde500665cb9f377f1c
Charles Clark <Charles.Clark@stonybrook.edu>
2022-09-07 17:40:35 -0400

    fixed deprecated imports and testing inline style

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import dash
-import dash_table
-from dash import html, dcc
+from dash import html, dcc, dash_table
 from dash.dependencies import Input, Output, State
 
 import plotly.graph_objs as go
+import plotly.express as px
 
 import pandas as pd
 
@@ -14,34 +14,41 @@ from example_table import table_dict
 df = pd.DataFrame(table_dict)
 
 # create table in plotly and display with dcc.Graph
-fig = go.Figure(data=[go.Table(
-    header=dict(values=list(df.columns)),
-    cells=dict(values=[df[col] for col in df.columns])
-)])
+# fig = go.Figure(data=[go.Table(
+#     header=dict(values=list(df.columns)),
+#     cells=dict(values=[df[col] for col in df.columns])
+# )])
+
+test_fig = px.scatter(x=[1,2,3], y=[1,2,3])
 
 app.layout = html.Div([
     html.H1('Example Table'),
-
-    dcc.Graph(figure=fig),
 
     html.P(id='selected-cell'),
     html.P(id='selected-cols'),
     html.Button("Swap Cols", id='swap-cols'),
     # create table using dash DataTable
-    dash_table.DataTable(
-        id='main-table',
-        data=df.to_dict('records'),
-        columns=[{"name": i, "id": i, "hideable": True, 'selectable': True} for i in df.columns],
-        hidden_columns=['scan_uid'],
-        sort_action='native',
-        column_selectable='multi',
+    html.Div(
+        dash_table.DataTable(
+            id='main-table',
+            data=df.to_dict('records'),
+            columns=[{"name": i, "id": i, "hideable": True, 'selectable': True} for i in df.columns],
+            hidden_columns=['scan_uid'],
+            sort_action='native',
+            column_selectable='multi',
 
-        # css styles
-        style_cell={
-            'textAlign': 'left',
-            'font-family': 'arial',
-        },
-        )
+            # css styles
+            style_cell={
+                'textAlign': 'left',
+                'font-family': 'arial',
+            },
+        ),
+        style={"display": "inline-block"}
+    ),
+    html.Div(
+        dcc.Graph(figure=test_fig),
+        style={"display": "inline-block"}
+    )
 ])
 
 
